c5cb8c5fca61ad32ab4528cac6463a0491f2f956
Voyager2k <clego22284@aol.com>
2013-11-03 23:00:20 -0800

    Re-add Perfomance Control and Advanced Settings
    Change-Id: Iea3c9a17f8ca8b185398d64dafdcb3b949dfc1ff

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -121,6 +121,8 @@ public class Settings extends PreferenceActivity
             R.id.system_section,
             R.id.date_time_settings,
             R.id.about_settings,
+            R.id.performance,
+            R.id.advanced_settings,
             R.id.accessibility_settings
     };
 
